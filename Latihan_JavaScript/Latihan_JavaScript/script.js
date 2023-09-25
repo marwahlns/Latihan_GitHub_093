dd6d09763baa966e5790a897749d8b3f62525a0e
marwahlns <marwahnisa1903@gmail.com>
2023-09-25 07:44:16 +0700

    Commit 1

---
+++ Latihan_JavaScript/Latihan_JavaScript/script.js
@@ -0,0 +1,58 @@
+﻿//function tambah() {
+//    let a = document.getElementById("bilangan1").value;
+//    let b = document.getElementById("bilangan2").value;
+//    document.getElementById("hasil").value = parseInt(a) + parseInt(b);
+//}
+//let tambah = function () {
+//    let a = document.getElementById("bilangan1").value;
+//    let b = document.getElementById("bilangan2").value;
+//    document.getElementById("hasil").value = parseInt(a) + parseInt(b);
+//}
+function cekangka() {
+    //const bil1 = document.getElementById("bilangan1").value;
+    //const bil2 = document.getElementById("bilangan2").value;
+
+    //if (bil1 < bil2) {
+    //    document.getElementById("output").innerHTML = bil1 + " lebih kecil dari pada " + bil2;
+    //} else if (bil1 > bil2) {
+    //    document.getElementsByClassName("myclass")[0].innerHTML = bil1 + " lebih besar dari pada " + bil2;
+    //} else {
+    //    document.getElementsByTagName("p")[0].innerHTML = bil1 + " sama dengan " + bil2;
+    //}
+
+    const bil1 = parseInt($("#bilangan1").val());
+    const bil2 = parseInt($("#bilangan2").val());
+
+    if (bil1 < bil2) {
+        $("#output").html(bil1 + " lebih kecil dari pada " + bil2);
+    } else if (bil1 > bil2) {
+        $(".myclass").html(bil1 + " lebih besar dari pada " + bil2);
+    } else {
+        $("p")[0].html(bil1 + " sama dengan " + bil2);
+    }
+}
+let tambah = () => {
+    let a = document.getElementById("bilangan1").value;
+    let b = document.getElementById("bilangan2").value;
+    document.getElementById("hasil").value = parseInt(a) + parseInt(b);
+}
+function kurang() {
+    let a = document.getElementById("bilangan1").value;
+    let b = document.getElementById("bilangan2").value;
+    document.getElementById("hasil").value = parseInt(a) - parseInt(b);
+}
+function kali() {
+    let a = document.getElementById("bilangan1").value;
+    let b = document.getElementById("bilangan2").value;
+    document.getElementById("hasil").value = parseInt(a) * parseInt(b);
+}
+function bagi() {
+    let a = document.getElementById("bilangan1").value;
+    let b = document.getElementById("bilangan2").value;
+    document.getElementById("hasil").value = parseInt(a) / parseInt(b);
+}
+function pangkat() {
+    let a = document.getElementById("bilangan1").value;
+    let b = document.getElementById("bilangan2").value;
+    document.getElementById("hasil").value = parseInt(a) ** parseInt(b);
+}
---
+++ Tugas_1_093/Tugas_1_093/Program.cs
@@ -0,0 +1,15 @@
+namespace Tugas_1_093
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
